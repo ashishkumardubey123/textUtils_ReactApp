fe7ba76dd3e490fce52def2766faeeb4bebfcd4b
ashishkumardubey123 <ashish9039062705@gmail.com>
2024-01-15 14:05:26 +0530

    this my first react aap

---
+++ precache-manifest.d87d08060ec3477b48537744450ceb95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b67ce4e66ada2eb9bb3948509b339d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15260033e46c73003758",
+    "url": "/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "4109fef235c2bd6de0eb",
+    "url": "/static/js/2.8dea5cbf.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "15260033e46c73003758",
+    "url": "/static/js/main.2359ccc7.chunk.js"
+  },
+  {
+    "revision": "8a5626378c6138c21833",
+    "url": "/static/js/runtime~main.cef9ab07.js"
+  }
+]);
